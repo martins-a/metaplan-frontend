d4ef056d19398824417bf5621654f50b7b6c1724
Atmartins <at-martins@outlook.com>
2025-07-14 22:29:42 -0300

    Added Jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Only transform .ts files
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!flat)/', // Exclude modules except 'flat' from transformation
+  ],
+};
